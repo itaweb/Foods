bd8111a3bd729bcb09d6f411226900d1fd88d51d
itaweb <admod32@gmail.com>
2021-01-24 00:45:26 +0300

    Add section number three

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ setInterval(() => {
   i++;
   const icon = document.querySelector(".section-1-icons .change");
   icon.classList.remove("change");
-  if (i > icons.length){
+  if (i > icons.length) {
     icons[0].classList.add("change");
-    i=1;
-  } else{
+    i = 1;
+  } else {
     icon.nextElementSibling.classList.add("change");
   }
-  
 }, 4000);
